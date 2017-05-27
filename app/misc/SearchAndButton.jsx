2c42b1516cab3110b48c016bc74f979021531c7a
Keven Kurz PC <keven.kurz@gmail.com>
2017-05-27 18:02:15 +0200

    tied  up input fields and buttons for step 4

--- app/misc/SearchAndButton.jsx
+++ app/misc/SearchAndButton.jsx
@@ -2,22 +2,38 @@ const React = require('react');
 const { Link } = require('react-router-dom');
 const PropTypes = require('prop-types');
 
-function Header(props) {
-  return (
-    <div className={props.className}>
-      <input
-        id="search"
-        placeholder="St. George, Utah"
-        type="text"
-        autoComplete="off"
-      />
-      <Link
-        className="button"
-        to={{ pathname: '/search' }}
-      >Get Weather</Link>
-    </div>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    const value = event.target.value;
+    this.setState(() => ({
+      searchString: value,
+    }));
+  }
+  render() {
+    return (
+      <div className={this.props.className}>
+        <input
+          id="search"
+          placeholder="St. George, Utah"
+          type="text"
+          autoComplete="off"
+          onChange={this.handleChange}
+        />
+        <Link
+          className="button"
+          to={{ pathname: '/search', search: `search=${this.state.searchString}` }}
+        >Get Weather</Link>
+      </div>
 
-  );
+    );
+  }
 }
 Header.propTypes = {
   className: PropTypes.string,
