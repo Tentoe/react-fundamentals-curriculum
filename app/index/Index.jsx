8a10c8b1d49d5a57479efa8005cf08f3e6c92bb3
Keven Kurz PC <keven.kurz@gmail.com>
2017-05-27 19:38:27 +0200

    removed container class from css

--- app/index/Index.jsx
+++ app/index/Index.jsx
@@ -3,14 +3,14 @@ const SearchAndButton = require('../misc/SearchAndButton');
 
 function Index() {
   return (
-    <div className="container">
-      <div className="content">
-        <form>
-          <label className="search-label" htmlFor="search">Enter a City and State</label>
-          <SearchAndButton className="index-search" />
-        </form>
-      </div>
+
+    <div className="content">
+      <form>
+        <label className="search-label" htmlFor="search">Enter a City and State</label>
+        <SearchAndButton className="index-search" />
+      </form>
     </div>
+
   );
 }
 module.exports = Index;
